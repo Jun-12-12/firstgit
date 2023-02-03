784cc89ba865892680da22b8941fd87b9cb9a1cf
Jun-12-12 <test20051516@gmail.com>
2023-02-03 14:26:16 +0900

    add new txet

--- Hello/src/Test.java
+++ Hello/src/Test.java
@@ -3,5 +3,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
+		System.out.println("Test!!");
 	}
 }
+
+// 1. 파일 코드 수정 -> 스테이징 -> 커밋 -> 푸시 -> 깃허브 사이트에서 푸시 성공 확인
+// 2. 깃허브 사이트 -> 코드 수정 -> 커밋 -> 로커 이클립스에서 pull
