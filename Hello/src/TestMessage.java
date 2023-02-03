a9b393224b4eda3a07aedebe5559d3edda7b087b
Jun-12-12 <test20051516@gmail.com>
2023-02-03 13:49:29 +0900

    First commit

---
+++ Hello/src/TestMessage.java
@@ -0,0 +1,10 @@
+
+public class TestMessage {
+
+	public TestMessage() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
